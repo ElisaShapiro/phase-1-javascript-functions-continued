458cdfe0c16091848e8b78ab805d08b4623d7b96
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-28 20:11:47 -0400

    commit tree 67f82b7239cec99e9063110372e230f601fe1c61

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`
+    } 
+}
+
+function mondayWork (work = "go to the office") {
+    if (work) {
+        return `This Monday, I will ${work}.`
+    }
+}
+
+function wrapAdjective(special = "*") {
+    return function (phrase = "a dedicated programmer") {
+        return (`You are ${special}${phrase}${special}!`)
+    }
+}
